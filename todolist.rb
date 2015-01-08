0df731ffd635c9018b2ed2e8c39e2b58669e09a9
Velsu <velius@poczta.onet.pl>
2015-01-08 16:11:41 +0100

    Startujemy

---
+++ todolist.rb
@@ -0,0 +1,70 @@
+require "./todoitem"
+
+class TodoList
+	attr_reader :name, :todo_items
+
+	def initialize(name)
+		@name = name
+		@todo_items = []
+	end
+
+	def add_item(name)
+		todo_items.push(TodoItem.new(name))
+	end
+
+	def remove_item(name)
+		if index = find_index(name)
+			todo_items.delete_at(index)
+			return true
+		else
+			return false
+		end
+	end
+
+	def set_complete(name)
+		if index = find_index(name)
+			todo_items[index].set_complete!
+			return true
+		else
+			return false
+		end
+	end
+
+
+	def find_index(name)
+		index = 0
+		found = false
+
+		todo_items.each do |item|
+			if item.name == name
+			found = true
+		end
+		if found
+			break
+		else
+			index += 1
+		end
+	end
+		if found
+			return index
+		else
+			return nil
+		end
+	end
+
+end
+
+
+todo_list = TodoList.new("Groceries")
+todo_list.add_item("Milk")
+todo_list.add_item("Eggs")
+todo_list.add_item("Bread")
+
+if todo_list.remove_item("Eggs")
+  puts "Eggs were removed from the list."
+end
+
+if todo_list.set_complete("Milk")
+  puts "Milk was marked as complete."
+end
+puts todo_list.inspect
